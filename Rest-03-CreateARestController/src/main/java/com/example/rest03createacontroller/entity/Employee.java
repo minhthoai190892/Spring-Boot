7543aefa375f0f4ddff881e96465ca978d3bc5cd
ASUS <minhthoai190892@gmail.com>
2023-04-14 08:04:44 +0700

    18-Create-an-entity-class

--- Rest-03-CreateARestController/src/main/java/com/example/rest03createacontroller/entity/Employee.java
+++ Rest-03-CreateARestController/src/main/java/com/example/rest03createacontroller/entity/Employee.java
@@ -2,6 +2,7 @@ package com.example.rest03createacontroller.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Data
@@ -10,18 +11,23 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
-
+@Entity
+@Table(name = "tbl_employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "name")
     // @JsonProperty("full_name") //thay đổi tên trường khi sử dụng
     private String name;
+    @Column(name = "age")
     // @JsonIgnore //đánh dấu ẩn tên trường
     private Long age;
+    @Column(name = "location")
     private String location;
+    @Column(name = "email")
     private String email;
+    @Column(name = "department")
     private String department;
 }
-
-
-
-
